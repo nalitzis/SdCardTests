fde7b3f0fa66639c13176ec0f7a1fe71fa9f4af0
Adolfo Bulfoni <adolfo.bulfoni@gmail.com>
2015-07-25 12:54:24 +0200

    added size of volumes

--- app/src/main/java/ado/testsdcard/MainActivity.java
+++ app/src/main/java/ado/testsdcard/MainActivity.java
@@ -2,6 +2,7 @@ package ado.testsdcard;
 
 import android.app.Activity;
 import android.os.Environment;
+import android.os.StatFs;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,11 +12,14 @@ import android.widget.TableRow;
 import android.widget.TextView;
 
 import java.io.File;
+import java.util.Formatter;
 
 
 public class MainActivity extends Activity {
 
     private TableLayout mRoot;
+
+    private static final float MB = 1024.0f * 1024.0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,8 @@ public class MainActivity extends Activity {
         mRoot = (TableLayout)findViewById(R.id.table_root);
         fillTxt();
     }
+
+
 
     private void fillTxt() {
         addHeader("android.app.Context API");
@@ -64,6 +70,33 @@ public class MainActivity extends Activity {
         for(int i = 0; i < files.length; i++) {
             addRow(i + ":  ", "" + Environment.isExternalStorageRemovable(files[i]));
         }
+
+        addHeader("\nandroid.os.StatFs API - available space");
+        addRow("[4.3] size of getFilesDir(): ", "" + printSize(getFilesDir()) + " MB");
+        addRow("[4.3] size of getExternalFilesDir(null): ", "" + printSize(getExternalFilesDir(null)) + " MB");
+        addRow("[4.3] size of Environment.getDataDirectory(): ", "" + printSize(Environment.getDataDirectory()) + " MB");
+        addRow("[4.3] size of Environment.getExternalStorageDirectory(): ", "" + printSize(Environment.getExternalStorageDirectory()) + " MB");
+
+    }
+
+    private long getSizeBytes(final File path) {
+        final StatFs stat = new StatFs(path.getPath());
+        long availableBytes = stat.getAvailableBytes();
+        return availableBytes;
+    }
+
+    private float toMB(final long bytes) {
+        return ((float)bytes)/MB;
+    }
+
+    private String toMBString(final long bytes) {
+        final float mb = toMB(bytes);
+        final Formatter formatter = new Formatter();
+        return formatter.format("%.2f", mb).toString();
+    }
+
+    private String printSize(final File path) {
+        return toMBString(getSizeBytes(path));
     }
 
     private void addHeader(String title) {
@@ -75,7 +108,7 @@ public class MainActivity extends Activity {
     private void addRow(String key, String value) {
         final TableRow tr = new TableRow(this);
         final TextView c2 = new TextView(this);
-        c2.setText(key + "   ");
+        c2.setText(key);
         final TextView c2val = new TextView(this);
         c2val.setText("" + value);
         tr.addView(c2);
